77e9535a24aa7f808b885f45d4c3a0f1e4cf47ef
Yubin <leepmh674735@gmail.com>
2026-03-25 09:42:24 +0900

    Yubin

---
+++ src/com/winter/exception/ExceptionEx.java
@@ -0,0 +1,16 @@
+package com.winter.exception;
+
+public class ExceptionEx {
+
+	
+	public static void main(String[] args) {
+		ExcePtionEx e1 = new ExceptionEx();
+		
+		e1.ex();
+	
+		  int a = 10;
+		  int b=0;
+		  try {
+			  
+	  }
+}
