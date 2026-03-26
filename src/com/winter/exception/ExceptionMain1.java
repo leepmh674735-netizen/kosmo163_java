00ad9ff1632293ef29af2694d2c918c2c64aba18
Yubin <leepmh674735@gmail.com>
2026-03-26 18:01:29 +0900

    26

---
+++ src/com/winter/exception/ExceptionMain1.java
@@ -0,0 +1,24 @@
+package com.winter.exception;
+
+public class ExceptionMain1 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 2;
+	   int c = a/b;
+	String str=null;
+	
+str.charAt(0);
+	System.out.println(args[0]);
+	   }catch(
+
+	ArithmeticException e)
+	e.printStackTrace();
+		e.printStackTrace();
+	   }catch( Exception e) {
+	  e.
+	{
+		// TODO: handle exception
+	}System.out.prinln("종료합니다");
+
+}
